a4d2db44a59670987e38e9f888b539d6b50efcb6
Rodrigo Sousa <rodrigos.comunica@gmail.com>
2021-05-20 20:22:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+const nome = "Rodrigo"
+let idade = 27 //que vai fazer em 2021, não sua idade atual.
+let aniversario = true
+const nasc = anoAtual - idade
+
+console.log(`Meu ano de nascimento é ${nasc}`)
+
+const iamhuman = true
+
+let x = 11
+let y = 8
+const soma = (x+y)
+
+console.log(`O resultado da soma de x e y é igual à ${soma}`)
